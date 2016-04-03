b62d4644de884a587e17ad29c9b0e054700e8bf4
Dale- <18717394542@163.com>
2016-04-04 00:25:08 +0800

    move the mock from json to localStorage

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,24 +1,38 @@
 var movieList = angular.module('MovieListModule', []);
-var i = 0;
+//var i = 0;
 
-movieList.controller('MovieListCtrl', function($scope, $http, $state, $stateParams, SetItem) {
-    i++;
-    $.getJSON('../mock/movies' + $stateParams.movieType + '.json',function(json){
-        if( i == 1 ) {
-            SetItem(json);
-        }
-        $scope.movies = JSON.parse(localStorage.getItem('movies'));
-    });
+movieList.controller('MovieListCtrl', function ($scope, $http, $state, $stateParams, SetItem) {
+    //i++;
+    //$.getJSON('../mock/movies' + $stateParams.movieType + '.json', function (json) {
+    //    if (i == 1) {
+    //        SetItem(json);
+    //    }
+    //    $scope.movies = JSON.parse(localStorage.getItem('movies'));
+    //});
+
+    $scope.movies = JSON.parse(localStorage.getItem('movies' + $stateParams.movieType));
+
 });
 
-movieList.controller('AddMovieCtrl', function($scope, $http, $state, $stateParams) {
+movieList.controller('AddMovieCtrl', function ($scope, $http, $state, $stateParams) {
 
-    $scope.saveMovie = function() {
-        var id = JSON.parse(localStorage.getItem('movies')).length;
-        $scope.movie = { name: $scope.name, pubTime:$scope.pubTime, director: $scope.director, star: $scope.star, type:"1", movieId: id + 1};
-        $scope.movies = JSON.parse(localStorage.getItem('movies'));
-        $scope.movies.push($scope.movie);
-        localStorage.setItem('movies', JSON.stringify($scope.movies));
+    $scope.saveMovie = function () {
+        var movie_type = $('#movie_type option:selected').val();
+        var movies = JSON.parse(localStorage.getItem('movies' + movie_type));
+        movies = movies ? movies : [];
+        var id = movies.length;
+        $scope.movie = {
+            name: $scope.name,
+            pubTime: $scope.pubTime,
+            director: $scope.director,
+            star: $scope.star,
+            type: movie_type,
+            movieId: id + 1
+        };
+
+        movies.push($scope.movie);
+        localStorage.setItem('movies' + movie_type, JSON.stringify(movies));
+        document.form.reset();
     };
 });
 
