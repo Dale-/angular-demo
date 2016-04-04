fc7cea5b87d4b0d1bbb330939f181adcfbea4b4a
Dale- <18717394542@163.com>
2016-04-04 23:37:00 +0800

    modify the format

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,24 @@
-var movie = angular.module('movieApp', ['ui.router', 'ngGrid', 'MovieListModule', 'timeFilter', 'movieServices']);
+angular.module('movieApp', ['ui.router', 'ngGrid', 'MovieListModule', 'timeFilter', 'movieServices']).config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/index');
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            views: {
+                '': {templateUrl: 'tpls/home.html'},
+                'main@index': {templateUrl: 'tpls/login.html'}
+            }
 
-movie.config(function($stateProvider, $urlRouterProvider) {
-   $urlRouterProvider.otherwise('/index');
-   $stateProvider
-       .state('index', {
-           url: '/index',
-           views: {
-               '': { templateUrl: 'tpls/home.html' },
-               'main@index': { templateUrl: 'tpls/login.html'}
-           }
-
-       })
-       .state('movieList', {
-           url: '/{movieType:[0-9]{1}}',
-           views: {
-               '': { templateUrl: 'tpls/movieList.html' },
-               'movieType@movieList': { templateUrl: 'tpls/movieType.html' },
-               'movieGrid@movieList' : { templateUrl: 'tpls/movieGrid.html' }
-           }
-       })
-       .state('addMovie', {
-          url: '/addMovie',
-           templateUrl: 'tpls/addMovie.html'
-       })
+        })
+        .state('movieList', {
+            url: '/{movieType:[0-9]{1}}',
+            views: {
+                '': {templateUrl: 'tpls/movieList.html'},
+                'movieType@movieList': {templateUrl: 'tpls/movieType.html'},
+                'movieGrid@movieList': {templateUrl: 'tpls/movieGrid.html'}
+            }
+        })
+        .state('addMovie', {
+            url: '/addMovie',
+            templateUrl: 'tpls/addMovie.html'
+        })
 });
