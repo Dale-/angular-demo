50ff6d4e84612865b6f711e6b6e857080548a744
Dale- <18717394542@163.com>
2016-04-04 23:31:54 +0800

    remove the defined module in global

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,15 +1,12 @@
-var movieList = angular.module('MovieListModule', []);
+angular.module('MovieListModule', [])
+    .controller('MovieListCtrl', function ($scope, $http, $state, $stateParams, GetMovies, DeleteMovie) {
 
-movieList.controller('MovieListCtrl', function ($scope, $http, $state, $stateParams, GetMovies, DeleteMovie) {
-
-    $scope.movieType = $stateParams.movieType;
-    $scope.movies = GetMovies($scope.movieType);
-    $scope.delete_movie = function (movie_id) {
-        DeleteMovie(movie_id, $scope.movies, $scope.movieType);
-    }
-});
-
-movieList.controller('AddMovieCtrl', function ($scope, $http, $state, $stateParams, GetMoviesByType, AddMovieToMovies) {
+        $scope.movieType = $stateParams.movieType;
+        $scope.movies = GetMovies($scope.movieType);
+        $scope.delete_movie = function (movie_id) {
+            DeleteMovie(movie_id, $scope.movies, $scope.movieType);
+        }
+    }).controller('AddMovieCtrl', function ($scope, $http, $state, $stateParams, GetMoviesByType, AddMovieToMovies) {
 
     $scope.saveMovie = function () {
         var movie_type = $('#movie_type option:selected').val();
